1105f11b06230471a2249d5b295e46f73d132956
Thomas Hallgren <thomas@tada.se>
2019-03-15 05:32:03 +0100

    Remove obsolete test directory

--- test/shared/handshake.go
+++
@@ -1,9 +0,0 @@
-package shared
-
-import "github.com/hashicorp/go-plugin"
-
-var Handshake = plugin.HandshakeConfig{
-	ProtocolVersion:  1,
-	MagicCookieKey:   "PLUGIN_MAGIC_COOKIE",
-	MagicCookieValue: "7468697320697320616e20616d617a696e67206d6167696320636f6f6b69652c206e6f6d206e6f6d206e6f6d",
-}
--- test/shared/hello.go
+++
@@ -1,5 +0,0 @@
-package shared
-
-type HelloApp interface {
-	Hello(msg string) string
-}
