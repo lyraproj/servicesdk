8d14f344e5190e491fdc6cf45f2c5ed7b236a728
Thomas Hallgren <thomas@tada.se>
2019-05-14 14:51:34 +0200

    Move Lyra::Do, Lyra::CRD, and Lyra::CRUD types from puppet-workflow
    This commit moves the pcore data types Lyra::Do, Lyra::CRD, and
    Lyra::CRUD types from the puppet-workflow module and replaces the
    former Puppet:: prefix with Lyra::.

--- wf/crd.go
+++ wf/crd.go
@@ -32,3 +32,35 @@ type CRUD interface {
 	// amended version of that state. The error NotFound is returned when no state can be found.
 	Update(externalId string, state px.OrderedMap) (px.OrderedMap, error)
 }
+
+var DoType px.Type
+var CrdType px.Type
+var CrudType px.Type
+
+func init() {
+	DoType = px.NewObjectType(`Lyra::Do`, `{
+		attributes => {
+      name => String
+    },
+    functions => {
+      do => Callable[[RichData,1], RichData]
+    }
+  }`)
+
+	CrdType = px.NewObjectType(`Lyra::CRD`, `{
+		attributes => {
+      name => String
+    },
+    functions => {
+      create => Callable[[Object], Tuple[Object,String]],
+      read   => Callable[[String], Object],
+      delete => Callable[[String], Boolean]
+    }
+  }`)
+
+	CrudType = px.NewObjectType(`Lyra::CRUD`, `Lyra::CRD{
+    functions => {
+      update => Callable[[String, Object], Object]
+    }
+  }`)
+}
