7895a840e645de35719ae5befc052889159d48d2
Thomas Hallgren <thomas@tada.se>
2019-06-17 08:56:00 +0200

    Remove error returns from Identity interface

--- serviceapi/identity.go
+++ serviceapi/identity.go
@@ -4,18 +4,18 @@ import "github.com/lyraproj/pcore/px"
 
 // Identity defines the API for services that track mappings between internal and external IDs
 type Identity interface {
-	BumpEra() error
-	ReadEra() (era int64, err error)
-	AddReference(internalId, otherId string) error
-	Associate(internalID string, externalID string) error
-	GetExternal(internalID string) (string, error)
-	GetInternal(externalID string) (string, error)
-	PurgeExternal(externalID string) error
-	PurgeInternal(internalID string) error
-	PurgeReferences(internalIDPrefix string) error
-	RemoveExternal(externalID string) error
-	RemoveInternal(internalID string) error
-	Search(internalIDPrefix string) (px.List, error)
-	Sweep(internalIDPrefix string) error
-	Garbage(internalIDPrefix string) (px.List, error)
+	BumpEra()
+	ReadEra() int64
+	AddReference(internalId, otherId string)
+	Associate(internalID string, externalID string)
+	GetExternal(internalID string) string
+	GetInternal(externalID string) string
+	PurgeExternal(externalID string)
+	PurgeInternal(internalID string)
+	PurgeReferences(internalIDPrefix string)
+	RemoveExternal(externalID string)
+	RemoveInternal(internalID string)
+	Search(internalIDPrefix string) px.List
+	Sweep(internalIDPrefix string)
+	Garbage(internalIDPrefix string) px.List
 }
