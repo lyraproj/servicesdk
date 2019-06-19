8a461879fe5ecc41c0c69c38a894b4106eabe00f
Thomas Hallgren <thomas@tada.se>
2019-06-19 16:45:20 +0200

    Let methods of Identity service take px.Context argument
    In order for the client side of the Identity service to use the same
    interface as the server, the interface methods must take a px.Context
    as their first parameter. This commit fixes that and also tweaks the
    methods GetInternal and GetExternal so that they return the result
    together with a boolean.

--- serviceapi/identity.go
+++ serviceapi/identity.go
@@ -4,18 +4,17 @@ import "github.com/lyraproj/pcore/px"
 
 // Identity defines the API for services that track mappings between internal and external IDs
 type Identity interface {
-	BumpEra()
-	ReadEra() int64
-	AddReference(internalId, otherId string)
-	Associate(internalID string, externalID string)
-	GetExternal(internalID string) string
-	GetInternal(externalID string) string
-	PurgeExternal(externalID string)
-	PurgeInternal(internalID string)
-	PurgeReferences(internalIDPrefix string)
-	RemoveExternal(externalID string)
-	RemoveInternal(internalID string)
-	Search(internalIDPrefix string) px.List
-	Sweep(internalIDPrefix string)
-	Garbage(internalIDPrefix string) px.List
+	BumpEra(px.Context)
+	AddReference(ctx px.Context, internalId, otherId string)
+	Associate(ctx px.Context, internalID string, externalID string)
+	GetExternal(ctx px.Context, internalID string) (string, bool)
+	GetInternal(ctx px.Context, externalID string) (string, bool)
+	PurgeExternal(ctx px.Context, externalID string)
+	PurgeInternal(ctx px.Context, internalID string)
+	PurgeReferences(ctx px.Context, internalIDPrefix string)
+	RemoveExternal(ctx px.Context, externalID string)
+	RemoveInternal(ctx px.Context, internalID string)
+	Search(ctx px.Context, internalIDPrefix string) px.List
+	Sweep(ctx px.Context, internalIDPrefix string)
+	Garbage(ctx px.Context, internalIDPrefix string) px.List
 }
